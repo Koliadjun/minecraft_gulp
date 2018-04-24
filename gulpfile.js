d0e906cac9fbf035bf556f4180dae558b8ace79c
Koiadjun <koliadjun@gmail.com>
2018-04-24 06:41:29 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var pug = require('gulp-pug');
+var server = require('gulp-server-livereload');
+var sourcemaps   = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+
+
+
+gulp.task('imgCopy', function(){
+	gulp.src('./dev/img/*')
+	.pipe(gulp.dest('./app/img/'))
+})
+
+gulp.task('sass', function () {
+  return gulp.src('./dev/scss/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.init())
+    .pipe(autoprefixer({
+            browsers: ['last 30 versions'],
+            cascade: false
+        }))
+    .pipe(sourcemaps.write('.'))
+    
+    .pipe(gulp.dest('./app/css'));
+});
+
+
+gulp.task('watch', function(){
+	gulp.watch('./dev/*.pug',['views'])
+	gulp.watch('./dev/pug-chunks/*.pug',['views'])
+  gulp.watch('./dev/scss/*.scss',['sass'])
+})
+ 
+
+gulp.task('webserver', function() {
+  gulp.src('app')
+    .pipe(server({
+      livereload: true,
+      directoryListing: false,
+      open: true
+    }));
+});
+
+gulp.task('views', function buildHTML() {
+  return gulp.src('./dev/*.pug')
+  .pipe(pug({
+    pretty: true
+  }))
+  .pipe(gulp.dest('./app/'))
+});
+
+gulp.task('default', ['watch','views','webserver', 'sass']);
